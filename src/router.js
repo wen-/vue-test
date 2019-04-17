2ddbc0a2a46060318ea6cb9a503354e3129b43cc
wen- <yellowwen@126.com>
2019-04-18 00:43:43 +0800

    增加登录页

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
@@ -11,7 +12,8 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: Home,
+      redirect: '/Login'
     },
     {
       path: "/about",
@@ -21,6 +23,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
     }
   ]
 });
---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-row type="flex" class="row-bg login-page" justify="center">
+    <div class="login-box">
+      <el-row type="flex" class="" justify="center"><i class="el-icon-menu login-img"></i></el-row>
+      <el-row type="flex" class="login-body" justify="center">
+        <el-form ref="form" :model="form" label-width="80px">
+          <el-form-item label="帐号">
+            <el-input placeholder="请输入帐号" v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input placeholder="请输入密码" v-model="form.password" show-password></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" :loading="false" @click="onSubmit">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-row>
+    </div>
+  </el-row>
+</template>
+
+<style scoped>
+.login-img{
+  font-size: 100px;
+  color: #409EFF;
+  margin: 100px 0 50px 92px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+    }
+  }
+}
+</script>
